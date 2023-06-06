e4699c6b94aed01f0ec01584fa66f5673b943e1e
André Nunes <andre.n@edu.sc.senai.br>
2023-06-06 20:58:26 -0300

    aula2

---
+++ aula2/teste-aula2/src/main/java/com/example/testeaula2/TesteAula2Application.java
@@ -0,0 +1,23 @@
+package com.example.testeaula2;
+
+import com.example.testeaula2.controller.ProdutoController;
+import com.example.testeaula2.model.Produto;
+import com.example.testeaula2.service.ProdutoService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAula2Application {
+
+	public static void main(String[] args) {
+
+//		ProdutoService produtoService = new ProdutoService(
+//				//ProdutoRepository
+//		);
+//
+//		ProdutoController produtoController = new ProdutoController(produtoService);
+//
+		SpringApplication.run(TesteAula2Application.class, args);
+	}
+
+}
