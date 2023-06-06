45e501f8f1c5f11a7046f66ca7ea427e9e90c6ef
André Nunes <andre.n@edu.sc.senai.br>
2023-06-05 22:02:23 -0300

    aula1-2

--- aula1/teste-aula1/src/main/java/org/example/Calculadora.java
+++ aula1/teste-aula1/src/main/java/org/example/Calculadora.java
@@ -2,7 +2,30 @@ package org.example;
 
 public class Calculadora {
 
+
+    //divisão, multiplicação, subtração, um fatorial 6! = 6*5*4*3*2*1
     public Double soma(Double valor1, Double valor2){
         return valor1+valor2;
+    }
+
+    public Double subtracao(Double valor1, Double valor2){
+        return valor1-valor2;
+    }
+
+    public Double divisao(Double valor1, Double valor2){
+        return valor1/valor2;
+    }
+
+    public Double multiplicacao(Double valor1, Double valor2){
+        return valor1*valor2;
+    }
+
+    public Double fatorial(Double valor){
+        double resultado = 1;
+        while (valor > 1){
+            resultado *= valor;
+            valor--;
+        }
+        return resultado;
     }
 }
--- aula1/teste-aula1/src/test/java/org/example/CalculadoraTest.java
+++ aula1/teste-aula1/src/test/java/org/example/CalculadoraTest.java
@@ -1,22 +1,49 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 // classe de testes
 class CalculadoraTest {
+    // criando uma nova instancia Calculador
+    Calculadora calculadora = new Calculadora();
 
     @Test // indica que um método é um teste e que pode ser executado individualmente
     void soma() {
-        // criando uma nova instancia Calculador
-        Calculadora calculadora = new Calculadora();
 
         // gerando o resultado da chamada do método soma()
         double resultado = calculadora.soma(1.1,2.0);
 
         // validando o resultado da chamada do soma
         assertEquals(3.1,resultado);
+    }
+
+    @Test
+    void subtracao() {
+        double resultado = calculadora.subtracao(3.2,2.1);
+
+        assertEquals(1.1,resultado);
+    }
+
+    @Test
+    void divisao() {
+        double resultado = calculadora.divisao(10.5,3.0);
+
+        assertEquals(3.5,resultado);
+    }
+
+    @Test
+    void multiplicacao() {
+        double resultado = calculadora.multiplicacao(3.1,2.0);
+
+        assertEquals(6.2,resultado);
+    }
+
+    @Test
+    void fatorial() {
+        double resultado = calculadora.fatorial(6.0);
+
+        assertEquals(720,resultado);
     }
 }
